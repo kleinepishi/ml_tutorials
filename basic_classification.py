4f256275059a72d645ee73d759fc30230a3f1888
Nathaniel <njmaki@mtu.edu>
2019-07-02 15:50:21 -0400

    update classification and regression

--- basic_classification.py
+++ basic_classification.py
@@ -13,9 +13,20 @@ print(tf.keras.__version__)
 
 # import fashion_mnist db
 fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+class_names = [
+    'T-shirt/top',
+    'Trouser',
+    'Pullover',
+    'Dress',
+    'Coat',
+    'Sandal',
+    'Shirt',
+    'Sneaker',
+    'Bag',
+    'Ankle boot']
 
 train_images.shape
 
@@ -39,9 +50,9 @@ train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 # display first 25 images from training set, with class name below image
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 for i in range(25):
-    plt.subplot(5,5,i+1)
+    plt.subplot(5, 5, i + 1)
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
@@ -51,25 +62,29 @@ plt.show()
 
 # basic building block: layer
 # extract representation from data fed to them
-# most DL is made up of chaining simple layers together (i.e tf.keras.layers.Dense)
+# most DL is made up of chaining simple layers together (i.e
+# tf.keras.layers.Dense)
 model = keras.Sequential([
-    # first layer transforms format of image from 2d array to 1d array of 28*28=784 pixels
+    # first layer transforms format of image from 2d array to 1d array of
+    # 28*28=784 pixels
     keras.layers.Flatten(input_shape=(28, 28)),
     # after flattening, dense (fully-connected) neural layers are used
     # first has 128 nodes (neurons)
     keras.layers.Dense(128, activation=tf.nn.relu),
     # second is 10 node softmax layer, returns 10 prob scores that sum to 1
-    # each node has a score, indicating prob that current img is a member of one of the 10 classes
+    # each node has a score, indicating prob that current img is a member of
+    # one of the 10 classes
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
 # now to compile the model
 # define loss function - how accurate the model is during training (want to minimize)
 # optimizer - how model is updated based on data it sees and loss function
-# metrics - used to monitor training and testing step (in this case, uses 'accuracy')
-model.compile(optimizer = 'adam',
-                loss = 'sparse_categorical_crossentropy',
-                metrics = ['accuracy'])
+# metrics - used to monitor training and testing step (in this case, uses
+# 'accuracy')
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 # start to train NNM
 # model learns to associate images and labels
@@ -97,6 +112,8 @@ print('Highest confidence:', np.argmax(predictions[0]))
 test_labels[0]
 
 # graph to observe entire set of 10 class predictions
+
+
 def plot_image(i, predictions_array, true_label, img):
     predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
     plt.grid(False)
@@ -110,36 +127,43 @@ def plot_image(i, predictions_array, true_label, img):
         color = 'blue'
     else:
         color = 'red'
-    
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],100*np.max(predictions_array),class_names[true_label]),color=color)
+
+    plt.xlabel(
+        "{} {:2.0f}% ({})".format(
+            class_names[predicted_label],
+            100 * np.max(predictions_array),
+            class_names[true_label]),
+        color=color)
+
 
 def plot_value_array(i, predictions_array, true_label):
-  predictions_array, true_label = predictions_array[i], true_label[i]
-  plt.grid(False)
-  plt.xticks([])
-  plt.yticks([])
-  thisplot = plt.bar(range(10), predictions_array, color="#777777")
-  plt.ylim([0, 1])
-  predicted_label = np.argmax(predictions_array)
-  
-  thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
 
 # now examine 0th image, predictions, and prediction array
 i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
 plot_image(i, predictions, test_labels, test_images)
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 plot_value_array(i, predictions, test_labels)
 plt.show()
 
 i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
+plt.figure(figsize=(6, 3))
+plt.subplot(1, 2, 1)
 plot_image(i, predictions, test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions,  test_labels)
+plt.subplot(1, 2, 2)
+plot_value_array(i, predictions, test_labels)
 plt.show()
 
 # next, plot several images with predictions
@@ -150,12 +174,12 @@ plt.show()
 # plot first x test images, predicted label, and true label
 num_rows = 5
 num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+num_images = num_rows * num_cols
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
 for i in range(num_images):
-    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
     plot_image(i, predictions, test_labels, test_images)
-    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
     plot_value_array(i, predictions, test_labels)
 plt.show()
 
@@ -164,7 +188,7 @@ img = test_labels[0]
 print(img.shape)
 
 # add image to a batch where it is only member
-img = (np.expand_dims(img,0))
+img = (np.expand_dims(img, 0))
 print(img.shape)
 
 # predict following image:
--- basic_regression.py
+++ basic_regression.py
@@ -13,15 +13,24 @@ from tensorflow.keras import layers
 print(tf.__version__)
 
 # download needed data
-dataset_path = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+dataset_path = keras.utils.get_file(
+    "auto-mpg.data",
+    "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
 dataset_path
 
 # import using pandas
-column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight',
-                'Acceleration', 'Model Year', 'Origin']
-raw_dataset = pd.read_csv(dataset_path, names=column_names,
-                      na_values = "?", comment='\t',
-                      sep=" ", skipinitialspace=True)
+column_names = ['MPG',
+                'Cylinders',
+                'Displacement',
+                'Horsepower',
+                'Weight',
+                'Acceleration',
+                'Model Year',
+                'Origin']
+raw_dataset = pd.read_csv(dataset_path,
+                          names=column_names,
+                          na_values="?", comment='\t',
+                          sep=" ", skipinitialspace=True)
 
 dataset = raw_dataset.copy()
 dataset.tail()
@@ -36,15 +45,19 @@ dataset = dataset.dropna()
 origin = dataset.pop('Origin')
 dataset['USA'] = (origin == 1) * 1.0
 dataset['Europe'] = (origin == 2) * 1.0
-dataset['Japan'] = (origin == 3)*1.0
+dataset['Japan'] = (origin == 3) * 1.0
 dataset.tail()
 
 # split data into train and testing sets
-train_dataset = dataset.sample(frac=0.8,random_state=0)
+train_dataset = dataset.sample(frac=0.8, random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
 
 # inspect data
-sns.pairplot(train_dataset[["MPG", "Cylinders", "Displacement", "Weight"]], diag_kind="kde")
+sns.pairplot(train_dataset[["MPG",
+                            "Cylinders",
+                            "Displacement",
+                            "Weight"]],
+             diag_kind="kde")
 
 # examine overal statistics
 train_stats = train_dataset.describe()
@@ -60,7 +73,31 @@ test_labels = test_dataset.pop('MPG')
 # important to normalize features that use different scales and ranges
 # without normalization, training is more difficult
 # causes dependancy on choice of units used as input
+
+
 def norm(x):
     return (x - train_stats['mean']) / train_stats['std']
+
+
 normed_train_data = norm(train_dataset)
 normed_test_data = norm(test_dataset)
+
+
+# build the model
+
+
+def build_model():
+    model = keras.Sequential([
+        layers.Dense(64, activation=tf.nn.relu, input_shape=[len(train_dataset.keys())]),
+        layers.Dense(64, activation=tf.nn.relu),
+        layers.Dense(1)
+    ])
+
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+
+    model.compile(loss='mean_squared_error',
+                  optimizer=optimizer,
+                  metrics=['mean_absolute_error', 'mean_squared_error'])
+    return model
+
+model = build_model()
